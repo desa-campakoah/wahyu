207e87914dbf769b42ec8d88e64a0c89254339d0
desa-campakoah <wahyulatifatunpbg@gmail.com>
2025-12-23 17:24:22 +0700

    Add files via upload

---
+++ data/Curug_15.js
@@ -0,0 +1,1 @@
+var json_Curug_15 = {"type":"FeatureCollection","name":"Curug_15","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","Nama":"Curug Tlahab"},"geometry":{"type":"Point","coordinates":[109.325777050446689,-7.29313489477067]}}]}
---
+++ data/Kesehatan_11.js
@@ -0,0 +1,1 @@
+var json_Kesehatan_11 = {"type":"FeatureCollection","name":"Kesehatan_11","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","Nama":"PKD Campakoah"},"geometry":{"type":"Point","coordinates":[109.321292436261302,-7.294980723743447]}}]}
